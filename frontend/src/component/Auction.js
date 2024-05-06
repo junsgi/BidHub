addd7a07828bf0e1395f519e12260bb34f39b4ce
junsgi <qwer3932@gmail.com>
2024-05-07 08:30:46 +0900

    0506

--- frontend/src/component/Auction.js
+++ frontend/src/component/Auction.js
@@ -5,11 +5,16 @@ const Auction = () => {
             <table className="Auction">
                 <tbody >
                     {
-                        [1, 2].map((d, i)=>{
+                        [1, 2, 3, 4, 5].map((d, i)=>{
                             return <Item key={i}/>
                         })
                     }
                 </tbody>
+                <tfoot>
+                    <tr colSpan = {2}>
+                        
+                    </tr>
+                </tfoot>
             </table>
         </>
     );
@@ -20,15 +25,17 @@ export default Auction;
 function Item() {
     return (
         <tr className="item" >
-            <td><img src={process.env.PUBLIC_URL + "/img/bidhub.png"}></img></td>
+            <td className="img">
+                <img src={process.env.PUBLIC_URL + "/img/bidhub.png"}></img>
+            </td>
             <td className="info">
                 <h4>제목</h4>
-                <div>
-                    <p>현재가</p>
-                    <p>적시 구매까</p>
+                <div className="price">
+                    <p>현재가 : </p>, 
+                    <p>즉시 구매가 : </p>
                 </div>
                 <div>
-                    남언 시간
+                    남은 시간
                 </div>
             </td>
         </tr>
--- frontend/src/component/MyPage.js
+++ frontend/src/component/MyPage.js
@@ -1,42 +1,87 @@
-import {Form, InputGroup} from 'react-bootstrap';
-import '../css/MyPage.css';
-import { useState } from 'react';
+import { Form, InputGroup, Button } from "react-bootstrap";
+import "../css/MyPage.css";
+import { useState } from "react";
 const MyPage = (props) => {
-    const SetStatus = props.SetStatus;
-    const NICK = sessionStorage.getItem("nickname") ?? "null";
-    const logout = () => {
-        sessionStorage.clear();
-        SetStatus("guest");
-    }
-    const [point, SetPoint] = useState(sessionStorage.getItem("point") ?? NaN);
+  const SetStatus = props.SetStatus;
+  const NICK = sessionStorage.getItem("nickname") ?? "null";
+  const logout = () => {
+    sessionStorage.clear();
+    SetStatus("guest");
+  };
+  const [point, SetPoint] = useState(sessionStorage.getItem("point") ?? NaN);
 
-    return (
-        <div className="mypage">
-            <h2>{NICK}님&nbsp;<sub className='logout' onClick={logout}>로그아웃</sub></h2>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">아이디</InputGroup.Text>
-                <Form.Control
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                    value = {sessionStorage.getItem("id")}
-                    disabled
-                    readOnly
-                />
-            </InputGroup>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">포인트</InputGroup.Text>
-                <Form.Control
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                    value = {point}
-                    disabled
-                    readOnly
-                />
-            </InputGroup>
-        </div>
-    );
-}
+  return (
+    <div className="mypage">
+      <div className="myInfo">
+        <h2>
+          {NICK}님&nbsp;
+          <sub className="logout" onClick={logout}>
+            로그아웃
+          </sub>
+        </h2>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon1">아이디</InputGroup.Text>
+          <Form.Control
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            value={sessionStorage.getItem("id")}
+            disabled
+            readOnly
+          />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon1">포인트</InputGroup.Text>
+          <Form.Control
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            value={point}
+            disabled
+            readOnly
+          />
+        </InputGroup>
+      </div>
+      <div className="btns">
+        <Button variant="dark">정보 수정</Button>
+        <Button variant="dark">경매 등록</Button>
+        <Button variant="dark">포인트</Button>
+      </div>
+      <table className="mySuccessTable">
+        <thead>
+          <th colSpan={2}>
+            <h3>내 낙찰 목록</h3>
+          </th>
+        </thead>
+        <tbody>
+          {
+            [1, 2].map((e, i) => {
+              return (
+                <MyItem key = {i}/>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default MyPage;
+
+const MyItem = () => {
+  return (
+    <tr className="item">
+      <td className="img">
+        <img src={process.env.PUBLIC_URL + "/img/bidhub.png"}></img>
+      </td>
+      <td className="info">
+        <h6>제목</h6>
+        <div className="price">
+          <div>낙찰 금액 : </div>
+          <div>낙찰 날짜 : </div>
+        </div>
+      </td>
+    </tr>
+  );
+};
