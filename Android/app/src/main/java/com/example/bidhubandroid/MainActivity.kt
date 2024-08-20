588360b3c1b40410d33343ea2add72a6cf250836
junsgi <qwer3932@gmail.com>
2024-08-21 01:50:56 +0900

    0812 android start

---
+++ Android/app/src/main/java/com/example/bidhubandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bidhubandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bidhubandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
